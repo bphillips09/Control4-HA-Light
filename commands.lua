83a8e7c43bef326d4e8477ffec01e97d7afc48e9
Aijay Adams <aijay.adams@gmail.com>
2023-11-26 15:46:58 -0800

    Less bad lookup into the XML

--- commands.lua
+++ commands.lua
@@ -29,14 +29,18 @@ end
 
 function RFP.PUSH_SCENE(idBinding, strCommand, tParams)
     local scene_value = C4:ParseXml(tParams.ELEMENTS)
-    C4:PersistSetValue("ALS:" .. tParams.SCENE_ID, scene_value["ChildNodes"][3]["Value"], false) 
-    print("Saving Advanced Lighting Scene " .. tParams.SCENE_ID)
+    C4:PersistSetValue("ALS:" .. tParams.SCENE_ID, scene_value, false) 
 end
 
 function RFP.ACTIVATE_SCENE(idBinding, strCommand, tParams)
     local scene_value = C4:PersistGetValue("ALS:" .. tParams.SCENE_ID, false) 
     print("Loading Advanced Lighting Scene " .. tParams.SCENE_ID)
-    SetLightValue(scene_value)
+    for _, v in pairs(scene_value["ChildNodes"]) do
+        if v["Name"] == "level" then
+            SetLightValue(v["Value"]) 
+            break
+        end
+    end
 end
 
 function RFP.ON(idBinding, strCommand, tParams)
